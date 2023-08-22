427c22384687463315709eff30593802f25a90dc
HO VAN <xengxucsoi@gmail.com>
2023-08-22 16:48:48 +0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
-import Chart from "./Chart";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  useEffect(() => {
+    // Update the page title based on the current time
+    const formattedTime = currentTime.toLocaleTimeString("en-US", {
+      hour12: true,
+    });
+    document.title = `Bây giờ là ${formattedTime}`;
+  }, [currentTime]);
+
+  const formattedTimeString = currentTime.toLocaleTimeString("en-US", {
+    hour12: true,
+  });
+
   return (
-    <div className="scale-75 flex justify-left items-center">
-      <Chart />
+    <div>
+      <h1 className="text-center">{formattedTimeString}</h1>
     </div>
   );
 }
