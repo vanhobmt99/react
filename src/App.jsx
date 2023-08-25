794d387a942f2a40d225b955333b00f62fea87d1
HO VAN <xengxucsoi@gmail.com>
2023-08-25 08:14:37 +0700

    dispatch

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,40 @@
-import { useState, useEffect } from "react";
+import { useReducer } from "react";
+
 
 function App() {
-  const [currentTime, setCurrentTime] = useState(new Date());
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setCurrentTime(new Date());
-    }, 1000);
-
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, []);
-
-  useEffect(() => {
-    // Update the page title based on the current time
-    const formattedTime = currentTime.toLocaleTimeString("en-US", {
-      hour12: true,
-    });
-    document.title = `Bây giờ là ${formattedTime}`;
-  }, [currentTime]);
-
-  const formattedTimeString = currentTime.toLocaleTimeString("en-US", {
-    hour12: true,
-  });
+  const reducer = (count, action) => {
+    switch (action) {
+      case 'Tang':
+        return count + 1;
+      case 'Giam':
+        return count - 1;
+      case 'Clear':
+        return 0;
+      default:
+        return count;
+    }
+  }
+const tang = () => {
+  dispatch({type:'Tang'})
+};
+const giam = () => {
+  dispatch({type:'Giam'})
+};
+const clear = () => {
+  dispatch({type:'Clear'})
+};
+  
+  const [count, dispatch] = useReducer(reducer, 0);
 
   return (
-    <div>
-      <h1 className="text-center">{formattedTimeString}</h1>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1>Count {count}</h1>
+      <div className="mt-4 space-x-4">
+        <button className="px-4 py-2 bg-blue-500 text-white rounded-lg" onClick={tang()}>Tang</button>
+        
+        <button className="px-4 py-2 bg-blue-500 text-white rounded-lg" onClick={clear()}>Clear</button>
+        <button className="px-4 py-2 bg-blue-500 text-white rounded-lg" onClick={giam()}>Giam</button>
+      </div>
     </div>
   );
 }
