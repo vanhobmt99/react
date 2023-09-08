2c913adef05eabfca440dad28d2975a512ab59a9
vanhobmt99 <57746121+vanhobmt99@users.noreply.github.com>
2023-09-09 06:14:43 +0700

    location and ip

--- src/App.jsx
+++ src/App.jsx
@@ -6,92 +6,109 @@ import google from "./google.png";
 
 function App() {
   const [currentTime, setCurrentTime] = useState(new Date());
+  const [clientIP, setClientIP] = useState("");
   const [serverLocation, setServerLocation] = useState("");
-  const [serverIP, setServerIP] = useState("");
+
+  useEffect(() => {
+
+    console.log("website render");
+    fetch("https://api.ipify.org?format=json")
+      .then((response) => response.json())
+      .then((data) => {
+        setClientIP(data.ip);
+      })
+      .catch((error) => {
+        console.error("Error fetching client IP:", error);
+      });
+
+    fetch("https://ipwho.is")
+      .then((response) => response.json())
+      .then((data) => {
+        setServerLocation(data.region);
+      })
+      .catch((error) => {
+        console.error("Error fetching server information:", error);
+      });
+  }, [serverLocation, clientIP]);
+
   useEffect(() => {
     setInterval(() => {
       setCurrentTime(new Date());
-      console.log("website render");
-      fetch("https://ipinfo.io/json")
-        .then((response) => response.json())
-        .then((data) => {
-          setServerLocation(data.city + ", " + data.region);
-          setServerIP(data.ip);
-        })
-        .catch((error) => {
-          console.error("Error fetching server information:", error);
-        });
     }, 1000);
   }, []);
 
-  const books = [
-    {
-      image: dummy1,
-      title: "Excel formulas",
-      highlights: [
-        "Chapter 1: Introduction to Excel",
-        "Chapter 2: Basic Formulas",
-        "Chapter 3: Advanced Functions",
-      ],
-    },
-    {
-      image: dummy2,
-      title: "Building a Mobile app",
-      highlights: [
-        "Chapter 1: App Design Principles",
-        "Chapter 2: Coding for Mobile",
-        "Chapter 3: Testing and Deployment",
-      ],
-    },
-    {
-      image: dummy3,
-      title: "Android App Development",
-      highlights: [
-        "Chapter 1: Getting Started with Android",
-        "Chapter 2: User Interface Design",
-        "Chapter 3: App Development Best Practices",
-      ],
-    },
-  ];
 
-  return (
-    <div className="w-4/5 m-auto border-solid border-2 border-sky-500 rounded-xl">
-      <div className="text-9xl text-green-700 font-bold p-10 pt-2 flex items-center justify-center">
-        <img src={google} alt="Google" className="mr-4" />
-        <div className="text-4xl text-center">Book for dummy</div>
-      </div>
-      <div className="flex flex-col justify-center md:flex-row">
-        {books.map((book, index) => (
-          <div
-            key={index}
-            className="m-10 p-5 bg-slate-100 rounded-lg hover:scale-105 text-center"
-          >
-            <img src={book.image} alt={`dummy${index + 1}`} />
-            <h1 className="m-2 font-bold">{book.title}</h1>
 
-            <ul className="text-left px-10">
-              {book.highlights.map((highlight, highlightIndex) => (
-                <li key={highlightIndex}>{highlight}</li>
-              ))}
-            </ul>
-          </div>
-        ))}
+
+const books = [
+  {
+    image: dummy1,
+    title: "Excel formulas",
+    highlights: [
+      "Chapter 1: Introduction to Excel",
+      "Chapter 2: Basic Formulas",
+      "Chapter 3: Advanced Functions",
+    ],
+  },
+  {
+    image: dummy2,
+    title: "Building a Mobile app",
+    highlights: [
+      "Chapter 1: App Design Principles",
+      "Chapter 2: Coding for Mobile",
+      "Chapter 3: Testing and Deployment",
+    ],
+  },
+  {
+    image: dummy3,
+    title: "Android App Development",
+    highlights: [
+      "Chapter 1: Getting Started with Android",
+      "Chapter 2: User Interface Design",
+      "Chapter 3: App Development Best Practices",
+    ],
+  },
+];
+
+return (
+  <div className="w-4/5 m-auto border-solid border-2 border-sky-500 rounded-xl">
+    <div className="text-9xl text-green-700 font-bold p-10 pt-2 flex items-center justify-center">
+      <img src={google} alt="Google" className="mr-4" />
+      <div className="text-4xl text-center">Book for dummy</div>
+    </div>
+    <div className="flex flex-col justify-center md:flex-row">
+      {books.map((book, index) => (
+        <div
+          key={index}
+          className="m-10 p-5 bg-slate-100 rounded-lg hover:scale-105 text-center">
+          <img src={book.image} alt={`dummy${index + 1}`} />
+          <h1 className="m-2 font-bold">{book.title}</h1>
+
+          <ul className="text-left px-10">
+            {book.highlights.map((highlight, highlightIndex) => (
+              <li key={highlightIndex}>{highlight}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+    <div>
+      <div className="text-4xl text-red-600 text-center font-bold mb-5 hover:scale-110">
+        About us
       </div>
       <div>
-        <div className="text-4xl text-red-600 text-center font-bold mb-5 hover:scale-110">
-          About us
+        <div className="flex flex-row items-center justify-center">
+          <h1 className="text-2xl font-bold mx-10">Client IP: {clientIP}</h1>
+          <h1 className="text-2xl font-bold   ">City IP: {serverLocation}</h1>
         </div>
-
-        <div className="text-2xl text-center font-bold mb-10">
-          Bây giờ là {currentTime.toLocaleTimeString()}
-        </div>
-        <div className="text-2xl font-bold mb-4">
-          Server Location: {serverLocation}
-        </div>
-        <div className="text-2xl font-bold">Server IP: {serverIP}</div>
+        <h1 className="text-2xl font-bold text-center ">Bây giờ là {currentTime.toLocaleTimeString()}</h1>
       </div>
+
     </div>
-  );
+
+  </div>
+
+);
 }
 
 export default App;
