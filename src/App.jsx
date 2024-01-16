a500ad972ed70c964bf8b424ca06f879eb67b902
vanhobmt99 <57746121+vanhobmt99@users.noreply.github.com>
2024-01-16 22:43:44 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -40,75 +40,85 @@ function App() {
 
 
 
-const books = [
-  {
-    image: dummy1,
-    title: "Excel formulas",
-    highlights: [
-      "Chapter 1: Introduction to Excel",
-      "Chapter 2: Basic Formulas",
-      "Chapter 3: Advanced Functions",
-    ],
-  },
-  {
-    image: dummy2,
-    title: "Building a Mobile app",
-    highlights: [
-      "Chapter 1: App Design Principles",
-      "Chapter 2: Coding for Mobile",
-      "Chapter 3: Testing and Deployment",
-    ],
-  },
-  {
-    image: dummy3,
-    title: "Android App Development",
-    highlights: [
-      "Chapter 1: Getting Started with Android",
-      "Chapter 2: User Interface Design",
-      "Chapter 3: App Development Best Practices",
-    ],
-  },
-];
+  const books = [
+    {
+      image: dummy1,
+      title: "Excel formulas",
+      highlights: [
+        "Chapter 1: Introduction to Excel",
+        "Chapter 2: Basic Formulas",
+        "Chapter 3: Advanced Functions",
+      ],
+    },
 
-return (
-  <div className="w-4/5 m-auto border-solid border-2 border-sky-500 rounded-xl">
-    <div className="text-9xl text-green-700 font-bold p-10 pt-2 flex items-center justify-center">
-      <img src={google} alt="Google" className="mr-4" />
-      <div className="text-4xl text-center">Book for dummy</div>
-    </div>
-    <div className="flex flex-col justify-center md:flex-row">
-      {books.map((book, index) => (
-        <div
-          key={index}
-          className="m-10 p-5 bg-slate-100 rounded-lg hover:scale-105 text-center">
-          <img src={book.image} alt={`dummy${index + 1}`} />
-          <h1 className="m-2 font-bold">{book.title}</h1>
 
-          <ul className="text-left px-10">
-            {book.highlights.map((highlight, highlightIndex) => (
-              <li key={highlightIndex}>{highlight}</li>
-            ))}
-          </ul>
+    {
+      image: dummy2,
+      title: "Building a Mobile app",
+      highlights: [
+        "Chapter 1: App Design Principles",
+        "Chapter 2: Coding for Mobile",
+        "Chapter 3: Testing and Deployment",
+      ],
+    },
+    {
+      image: dummy3,
+      title: "Android App Development",
+      highlights: [
+        "Chapter 1: Getting Started with Android",
+        "Chapter 2: User Interface Design",
+        "Chapter 3: App Development Best Practices",
+      ],
+    },
+
+  ];
+
+  return (
+
+
+    <div className=" m-auto border-solid border-2 border-sky-500 rounded-xl ">
+
+<div className="fixed top-0 left-1/2 transform -translate-x-1/2 z-10 text-9xl text-green-700 font-bold p-10 pt-2">
+
+        <div className="flex items-center">
+          <img src={google} alt="Google" className="mr-4" />
+          <div className="text-4xl text-center">Book for dummy</div>
         </div>
-      ))}
-    </div>
-    <div>
-      <div className="text-4xl text-red-600 text-center font-bold mb-5 hover:scale-110">
-        About us
+      </div>
+
+
+      <div className="flex mt-20 flex-wrap justify-left flex-row ">
+        {books.map((book, index) => (
+          <div
+            key={index}
+            className="m-4 px-2 py-10 bg-teal-400 rounded-lg hover:scale-105 w-1/3">
+
+            <img src={book.image} alt={`dummy${index + 1}`} className="w-[18em] h-[18em] mx-auto" />
+            <h1 className="m-1 font-bold text-center">{book.title}</h1>
+            <ul className="text-center px-0">
+              {book.highlights.map((highlight, highlightIndex) => (
+                <li key={highlightIndex}>{highlight}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
       </div>
       <div>
-        <div className="flex flex-row items-center justify-center">
-          <h1 className="text-2xl font-bold mx-10">Client IP: {clientIP}</h1>
-          <h1 className="text-2xl font-bold   ">City IP: {serverLocation}</h1>
+        <div className="text-4xl text-red-600 text-center font-bold mb-5 hover:scale-110">
+          About us
         </div>
-        <h1 className="text-2xl font-bold text-center ">Bây giờ là {currentTime.toLocaleTimeString()}</h1>
+        <div>
+          <div className="flex flex-row items-center justify-center">
+            <h1 className="text-2xl font-bold mx-10">Client IP: {clientIP}</h1>
+            <h1 className="text-2xl font-bold">City IP: {serverLocation}</h1>
+          </div>
+          <h1 className="text-2xl font-bold text-center ">
+            Bây giờ là {currentTime.toLocaleTimeString()}
+          </h1>
+        </div>
       </div>
-
     </div>
-
-  </div>
-
-);
+  );
 }
 
 export default App;
